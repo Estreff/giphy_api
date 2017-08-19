0477b154bbef81e11aa3a52af64e09837b52dc81
Erik Streff <Estreff@gmail.com>
2017-08-18 21:12:12 -0600

    Completed with duplicate verification, number adjust and rating

--- javascript/api.js
+++ javascript/api.js
@@ -4,11 +4,15 @@ $(function() {
 var topics = ['Val Kilmer', 'Kurt Russel', 'Tom Hanks', 'Kate Beckinsale', 'Tom Cruise', 'Cate Blanchett'];
 var newSelection;
 var gifNumber = 3;
+var gifNumberChanged;
+var gifMax = 25;
+
 
 // Function to use the topics Array to create Buttons
 	function createButton() {
 		$('#buttons').html("")
-		$('#qtyNeeded').attr('placeholder', gifNumber);
+		$('#qtyNeeded').attr('value', gifNumber);
+		$('#qtyNeeded').attr('max', 25);
 
 		for (var i = 0; i < topics.length; i++) {
 			var createTopicBtn = topics[i];
@@ -27,68 +31,13 @@ var gifNumber = 3;
 // Call Create Buttons to push to the website
 	createButton();
 
-// When a new topic is added this will add to array and create clickable button
-	function newTopic() {
-		newSelection = $('#topicInput').val();
-		console.log('New Topic: ', newSelection);
-		if(newSelection === '') {
-			alert('Please enter name!');
-		} else {
-			topics.push(newSelection);
-			console.log('New Topic Array: ', topics);
-			createButton();
-			$('#topicInput').val("");
-		}	
-	}
 
-// When 'Add Topic!' Button is selected, it adds new Topic into topics Array
-	$('#addTopic').click(function() {
-		newTopic()
-	});
-
-// When Enter is hit on Topic Button, it adds new Topic into topics Array
-
-	$('#topicInput').keypress(function(e) {
-		if(e.which == 13) {
-			newTopic()
-		}
-	});
-
-// Selecting the quantity of Gif files to return 
-
-	function selectQty() {
-		console.log('Qty Start: ', gifNumber)	
-		gifNumber = $('#qtyNeeded').val();
-		if($('#qtyNeeded').val() === "") {
-			alert('Enter Quantity');
-		} else {
-			console.log('New Quantity: ', gifNumber);
-			$('#qtyNeeded').val("");
-			$('#qtyNeeded').attr('placeholder', gifNumber);
-					
-		}
-	}
-
-// When 'Submit' Button is selected, it submits qty for use with API query
-	$('#addQty').click(function() {
-		selectQty();
-	});
-
-// When Enter is hit on Quantity Select, it submits qty for use with API query
-	$('#addQty').keypress(function(f) {
-		if(f.wvhich == 13) {
-			alert('Enter Quantity Selected');
-			selectQty()
-		}
-	});
-
-
-// Giphy Actors API Key Api Key: 65e85c3c72d046378ec61d27120944aa
 
 var createGif;
-var createIframe;
-var topicName;
+var topicName = "";
+var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=65e85c3c72d046378ec61d27120944aa&q=' + topicName +'&limit=' + gifNumber + '&offset=0&rating=PG&lang=en';
 
+// Giphy Actors API Key Api Key: 65e85c3c72d046378ec61d27120944aa
 // Topic Button selected and will reach out to Giphy API
 	function topicButton(event) {
 			$('#gifs1').html("");
@@ -97,8 +46,7 @@ var topicName;
 			topicName = $(event.currentTarget).html();
 			console.log('Topic Name: ', topicName);
 			$('#gifsTitle').html(topicName).css('visibility', 'visible');
-	
-			var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=65e85c3c72d046378ec61d27120944aa&q=' + topicName +'&limit=' + gifNumber + '&offset=0&rating=PG&lang=en';
+			queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=65e85c3c72d046378ec61d27120944aa&q=' + topicName +'&limit=' + gifNumber + '&offset=0&rating=PG&lang=en';
 			$.ajax({
 				url:queryURL,
 				method: "GET"	
@@ -106,34 +54,167 @@ var topicName;
 				console.log(response);	
 				for (var j = 0; j < response.data.length/2; j++) {
 					createGif = response.data[j].images.original_still.url;
-					createImg = $(`<img src="${createGif}" width="100%" data-still="${response.data[j].images.original_still.url}" data-animate="${response.data[j].images.original.url}"></img>`);
-					$('#gifs1').append(createImg);
+					createImg = $(`<p class="caption"><img src="${createGif}" width="100%" data-still="${response.data[j].images.original_still.url}" data-animate="${response.data[j].images.original.url}"></img>Gif Rating: ${response.data[j].rating}</p>`)
+				$('#gifs1').append(createImg);
 				}
 				for (var k = Math.round(response.data.length/2); k < response.data.length; k++) {
 					createGif = response.data[k].images.original_still.url;
-					createImg = $(`<img src="${createGif}" width="100%" data-still="${response.data[k].images.original_still.url}" data-animate="${response.data[k].images.original.url}"></img>`);
+					createImg = $(`<p class="caption"><img src="${createGif}" width="100%" data-still="${response.data[k].images.original_still.url}" data-animate="${response.data[k].images.original.url}"></img>Gif Rating: ${response.data[k].rating}</p>`);
 					$('#gifs2').append(createImg);
 				}
 			})
+		}
+var topicExists = false;
+var updatedSelection = "";
 
-							
-	};
+// When a new topic is added this will add to array and create clickable button
+	function newTopic() {
+		newSelection = $('#topicInput').val();
+		console.log('New Topic: ', newSelection);
 
-			$('#gifGroup').click(function(event) {
-				var gifSelected =  $(event.target);
-				var gifSelectedSRC = $(event.target).attr('src');
-				console.log('Gif selected: ', gifSelectedSRC);
-				var still =  $(event.target).attr('data-still');
-				var animate =  $(event.target).attr('data-animate');
-				
-				if (gifSelectedSRC === still) {
-					gifSelected.attr('src', animate);
-					console.log('Gif executed: ', animate)
-				} else {
-					gifSelected.attr('src', still);
-					console.log('Gif executed: ', still)
+				function titleCase(newSelection) {
+					newSelection = newSelection.split(" ");
+					console.log('Before For Loop: ', newSelection);
+						for(var m = 0; m < newSelection.length; m++) {
+							newSelection[m] = newSelection[m].charAt(0).toUpperCase() + newSelection[m].slice(1);
+							console.log(newSelection[m]);
+						}
+					console.log('After For Loop: ', newSelection)
+					updatedSelection = newSelection.join(" ");
+					console.log('After Join: ', newSelection);
 				}
-					
-			});
+				
+				for(var l = 0; l < topics.length; l++) {
+					if (newSelection.toLowerCase() == topics[l].toLowerCase()) {
+						topicExists = true;
+						console.log(topicExists);
+						console.log(topics); }
+				}
+						
+
+			if(newSelection == "") {
+					alert('Please enter name!');
+				} else if (topicExists === true) {
+					alert('Topic Entered already exists, please make another entry!');
+					$('#topicInput').val("");
+					topicExists = false;
+				} else {
+					titleCase(newSelection);
+					topics.push(updatedSelection);
+					console.log('New Topic Array: ', topics);
+					createButton();
+					$('#gifsTitle').text(updatedSelection);
+					$('#topicInput').val("");
+				}
+			}
+	
+	// .split(" ").charAt[0].toUpperCase().join(" ")
+
+
+
+// Selecting the quantity of Gif files to return 
+
+	function selectQty() {	
+		gifNumberChanged = $('#qtyNeeded').val();		
+		
+		if($('#qtyNeeded').val() == "" || $('#qtyNeeded').val() === 0) {
+			alert('Enter Quantity');
+			$('#qtyNeeded').attr('value', gifNumber);
+		} else if (gifNumberChanged <= gifMax) {
+			$('#qtyNeeded').attr('value', gifNumberChanged);	
+			gifNumber = gifNumberChanged;		
+		} else {
+			alert(`Please select a number less than or equal to ${gifMax}`);
+		}
+	}
+
+  // Reloads ajax call to update the quantity of gif files that are displayed
+	 // and Displays New topic when created!
+
+		function qtyReload() {
+			$('#gifs1').html("");
+			$('#gifs2').html("");
+			reloadTopicName = $('#gifsTitle').text();
+			console.log(reloadTopicName);
+			reloadGifNumber = $('#qtyNeeded').attr('value');
+
+			if(reloadTopicName != "") {
+				$('#gifsTitle').html(reloadTopicName).css('visibility', 'visible');
+			} else {
+				$('#gifsTitle').html(reloadTopicName).css('visibility', 'hidden');
+			}
+
+			console.log('Reload Topic: ', reloadTopicName);
+			console.log('Reload Gif Number', reloadGifNumber);
+			var reloadQueryURL = 'https://api.giphy.com/v1/gifs/search?api_key=65e85c3c72d046378ec61d27120944aa&q=' + reloadTopicName +'&limit=' + reloadGifNumber + '&offset=0&rating=PG&lang=en';
+			$.ajax({
+				url:reloadQueryURL,
+				method: "GET"	
+			}).done(function(reloadResponse) {
+				console.log(reloadResponse);	
+				for (var j = 0; j < reloadResponse.data.length/2; j++) {
+					createGif = reloadResponse.data[j].images.original_still.url;
+					createImg = $(`<p class="caption"><img src="${createGif}" width="100%" data-still="${reloadResponse.data[j].images.original_still.url}" data-animate="${reloadResponse.data[j].images.original.url}"></img>Gif Rating: ${reloadResponse.data[j].rating}</p>`);					
+					$('#gifs1').append(createImg);
+				}
+				for (var k = Math.round(reloadResponse.data.length/2); k < reloadResponse.data.length; k++) {
+					createGif = reloadResponse.data[k].images.original_still.url;
+					createImg = $(`<p class="caption"><img src="${createGif}" width="100%" data-still="${reloadResponse.data[k].images.original_still.url}" data-animate="${reloadResponse.data[k].images.original.url}"></img>Gif Rating: ${reloadResponse.data[k].rating}</p>`);
+					$('#gifs2').append(createImg);
+				}
+			})
+		}
+
+// Button and Enter Selection for changing Quantities!!! 
+
+// When 'Submit' Button is selected, it submits qty for use with API query
+	$('#addQty').click(function() {
+		selectQty();
+		qtyReload();
+	});
+
+// When Enter is hit on Quantity Select, it submits qty for use with API query
+	$('#qtyNeeded').keypress(function(f) {
+		if(f.which == 13) {
+			selectQty();
+			qtyReload();			
+		}
+	});
+
+// Button and Enter Selection for Adding Topics!!!
+
+// When 'Add Topic!' Button is selected, it adds new Topic into topics Array
+	$('#addTopic').click(function() {
+		newTopic();
+		qtyReload();		
+	});
+
+// When Enter is hit on Topic Button, it adds new Topic into topics Array
+
+	$('#topicInput').keypress(function(e) {
+		if(e.which == 13) {
+			newTopic();
+			qtyReload();
+		}
+	});
+						
+	
+// Switched the img src from 'Still' to 'Animated' after loaded
+	$('.gifCatalog').click(function(event) {
+		var gifSelected =  $(event.target);
+		var gifSelectedSRC = $(event.target).attr('src');
+		console.log('Gif selected: ', gifSelectedSRC);
+		var still =  $(event.target).attr('data-still');
+		var animate =  $(event.target).attr('data-animate');
+		
+		if (gifSelectedSRC === still) {
+			gifSelected.attr('src', animate);
+			console.log('Gif executed: ', animate)
+		} else {
+			gifSelected.attr('src', still);
+			console.log('Gif executed: ', still)
+		}
+			
+	});
 
 });
